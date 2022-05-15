165192a233c6dbb9114c7f8a23f38cf12be6b658
Aaron S Grincewicz <70443410+ASGrincewicz@users.noreply.github.com>
2022-05-15 00:17:29 -0400

    Update Mover.h

--- Mover.h
+++ Mover.h
@@ -1,12 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "Mover.generated.h"
-
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UMover : public UActorComponent
@@ -14,16 +10,15 @@ class CRYPTRAIDER_API UMover : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UMover();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void SetShouldMove(bool ShouldMove);
 	
 private:
 
@@ -32,12 +27,8 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float MoveTime = 4;
-
-	UPROPERTY(EditAnywhere)
-	bool ShouldMove = false;
-
-	FVector OriginalLocation;
 	
-
-		
+	bool ShouldMove = false;
+	
+	FVector OriginalLocation;
 };
